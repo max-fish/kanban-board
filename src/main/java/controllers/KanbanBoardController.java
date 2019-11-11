14f6715fefefed21015796fc3baaec5f23ea6750
Anna Cachadina <k1889050@kcl.ac.uk>
2019-11-11 17:09:56 +0000

    Add columns functionality

--- src/main/java/controllers/KanbanBoardController.java
+++ src/main/java/controllers/KanbanBoardController.java
@@ -1,15 +1,27 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import ui.KanbanColumn;
+
+import java.io.IOException;
+
 
 public class KanbanBoardController {
+    @FXML
+    private HBox columns;
+
+
     @FXML
     public void goToHomeScreen(){
         //TODO implement action to home screen
     }
 
     @FXML
-    public void makeNewColumn(){
-        //TODO implement making a new column
+    public void makeNewColumn() throws IOException
+    {
+        KanbanColumn column = new KanbanColumn();
+        columns.getChildren().add(column.create());
+
     }
 }
