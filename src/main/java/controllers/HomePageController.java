cd83d396fa7ea396a78a3fd7f41adb23cb64e735
Kamil Kuzara <kamil.kuzara@kcl.ac.uk>
2019-11-20 08:10:38 +0100

    Add an import/export button to the home page

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,11 +1,12 @@
 package controllers;
 
+import com.jfoenix.controls.JFXPopup;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.Initializable;
 import ui.KanbanBoard;
 import model.KanbanModel;
 import model.Board;
-import model.Column;
 import utils.ComponentMaker;
-import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -13,19 +14,27 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class HomePageController {
+public class HomePageController implements Initializable {
     @FXML
     private BorderPane rootPane;
     @FXML
     private GridPane boardGrid;
+    @FXML
+    private JFXButton fileMenuButton;
     private int colCounter = 0;
     private int rowCounter = 0;
+    private JFXPopup fileMenu;
 
-    public void initConfig(){
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         boardGrid.maxWidthProperty().bind(rootPane.widthProperty().multiply(4).divide(5));
         boardGrid.maxHeightProperty().bind(rootPane.heightProperty().multiply(4).divide(5));
+
+        fileMenu = ComponentMaker.makeFileMenu();
     }
 
     @FXML
@@ -37,14 +46,6 @@ public class HomePageController {
 
     @FXML
     public void makeNewBoard() {
-        Label boardLabel = new Label("New Board");
-        Board boardModel = new Board(boardLabel.getText());
-
-        makeNewBoard(boardModel, boardLabel);
-    }
-
-    public void makeNewBoard(Board boardModel, Label boardLabel)
-    {
         try {
             KanbanBoard board = new KanbanBoard();
 
@@ -52,8 +53,10 @@ public class HomePageController {
                 rowCounter++;
                 colCounter = 0;
             }
+            Label boardLabel = new Label("New Board");
             StackPane newBoardCard = ComponentMaker.makeBoardCard(boardLabel);
 
+            Board boardModel = new Board(boardLabel.getText());
             KanbanModel.instance().addBoard(boardModel);
 
             board.getController().setBoard(boardModel);
@@ -62,35 +65,19 @@ public class HomePageController {
             board.getController().setHomePageLabel(boardLabel);
             board.getController().setTitleChangeListener();
 
-            if(boardModel.hasColumns())
-                createColumns(boardModel, board);
-
             newBoardCard.setOnMouseClicked(event -> { rootPane.setCenter(board); });
 
             boardGrid.add(newBoardCard, colCounter, rowCounter);
             colCounter++;
         } catch (IOException e) {
-            System.out.println("The board could not be created.");
             e.printStackTrace();
         }
     }
 
-    private void createColumns(Board boardModel, KanbanBoard board)
-    {
-        List<Column> columns = boardModel.getColumns();
-        for(Column column : columns)
-            board.getController().makeNewColumn(column);
-    }
-
     @FXML
-    public void loadJSON()
+    public void openFileMenu()
     {
-        KanbanModel.instance().loadJSON();
-    }
-
-    @FXML
-    public void saveJSON()
-    {
-        KanbanModel.instance().saveJSON();
+        fileMenu.show(fileMenuButton, JFXPopup.PopupVPosition.TOP,
+                      JFXPopup.PopupHPosition.LEFT, 0, fileMenuButton.getHeight());
     }
 }
