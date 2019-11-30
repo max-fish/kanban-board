6311674f57741b991f62a1c318fb3a0fd6bdae72
KHruthik <hketepalli@gmail.com>
2019-11-30 12:31:58 +0000

    Start UI testing

--- src/test/java/TestBoard.java
+++ src/test/java/TestBoard.java
@@ -1,20 +1,29 @@
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
-
+import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.robot.Motion;
-
+import static org.testfx.assertions.api.Assertions.assertThat;
+import utils.ComponentMaker;
 import ui.HomePage;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class TestBoard extends ApplicationTest {
+    
 
     @Override 
     public void start(Stage primaryStage) throws IOException {
         new HomePage().start(primaryStage);
+    }
+
+    @Test
+    public void test1() {
+        clickOn("#addButton");
+        targetWindow("Name your board");
+        //assertThat(targetWindow("Name your board").lookup("#okButton").queryButton()).hasText("Ok");
     }
 }
