5203efac5f27ebfe76be790e6da4826efd802c2a
KHruthik <hruthik.ketepalli@kcl.ac.uk>
2019-12-09 12:15:01 +0000

    Add multiple testing functionality
    It is now possible to run more than one test.

--- src/test/java/TestBoard.java
+++ src/test/java/TestBoard.java
@@ -2,16 +2,39 @@ import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
 
 import ui.HomePage;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
 public class TestBoard extends ApplicationTest {
+
+    @Override
+    public void init() throws Exception {
+        FxToolkit.registerStage(() -> new Stage());
+    }
 
     @Override 
     public void start(Stage primaryStage) throws IOException {
         new HomePage().start(primaryStage);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        FxToolkit.hideStage();
     }
 
     @Test
