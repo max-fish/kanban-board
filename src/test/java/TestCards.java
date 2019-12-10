85c60f106c3c7b304190ab6914f8ba0102ecf810
KHruthik <hruthik.ketepalli@kcl.ac.uk>
2019-12-10 15:34:09 +0000

    Complete tests on column, card and board

--- src/test/java/TestCards.java
+++ src/test/java/TestCards.java
@@ -10,11 +10,48 @@ public class TestCards extends BaseTest
     }
 
     @Test
-    public void TestIfCardNameCanBeAdded() {
+    public void TestIfCardNameCanBeEditied() {
         createCard();
 
-        clickOn("Add Card Title");
+        //Move to end of text
+        moveTo("New KanbanCard");
+        moveBy(50,0);
+        clickOn(MouseButton.PRIMARY);
+
+        eraseText(14);
         write("Test");
+        sleep(500);
+    }
+
+    @Test
+    public void testIfExtraDetailsCanBeAddedToCard() {
+        createCard();
+
+        //Click on edit button
+        moveTo("New KanbanCard");
+        moveBy(-93, -53);
+        clickOn(MouseButton.PRIMARY);
+
+        //Move to description box
+        moveTo("Task description:");
+        moveBy(0,20);
+        clickOn(MouseButton.PRIMARY);
+
+        write("This is a test.");
+
+        //Change story point
+        clickOn("1");
+        clickOn("3");
+
+        clickOn("Save");
+
+        //Verify that details are saved
+        moveTo("New KanbanCard");
+        moveBy(-93, -53);
+        clickOn(MouseButton.PRIMARY);
+
+        moveTo("This is a test.");
+
         sleep(500);
     }
 
@@ -22,11 +59,59 @@ public class TestCards extends BaseTest
     public void TestDeleteCard() {
         createCard();
 
-        //Move to delete button
-        moveTo("Add Card Title");
-        moveBy(92, -17);
-
+        moveBy(100,100);
         clickOn(MouseButton.PRIMARY);
+
+        //Move to delete button
+        moveTo("New KanbanCard");
+        moveBy(93, -53);
+        clickOn(MouseButton.PRIMARY);
+        
+        sleep(500);
+    }
+
+    @Test
+    public void testIfCardCanBeDragedInSameColumn() {
+        createCard();
+
+        //Move to end of text
+        moveTo("New KanbanCard");
+        moveBy(50,0);
+        clickOn(MouseButton.PRIMARY);
+
+        eraseText(14);
+        write("Test");
+
+        clickOn("#columnMenuButton");
+
+        clickOn("Add card");
+
+        moveBy(100,100);
+        clickOn(MouseButton.PRIMARY);
+
+        moveTo("Test");
+        moveBy(0,-20);
+
+        press(MouseButton.PRIMARY);
+        moveBy(0,150);
+        release(MouseButton.PRIMARY);
+
+        sleep(500);
+    }
+
+    @Test
+    public void TestIfCardCanBeDraggedToNextColumn() {
+        createCard();
+
+        clickOn("#addColumn");
+
+        moveTo("New KanbanCard");
+        moveBy(0,-20);
+
+        press(MouseButton.PRIMARY);
+        moveBy(250,0);
+        release(MouseButton.PRIMARY);
+
         sleep(500);
     }
 }
