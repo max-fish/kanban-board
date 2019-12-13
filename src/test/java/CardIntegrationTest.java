47ca038e37cb87dee9db4e8389103230df6ad2e5
KHruthik <hruthik.ketepalli@kcl.ac.uk>
2019-12-13 18:14:09 +0000

    Add card integration testing

---
+++ src/test/java/CardIntegrationTest.java
@@ -0,0 +1,44 @@
+import data.model.BoardModel;
+import data.model.ColumnModel;
+import data.model.CardModel;
+
+import utils.Constants.ColumnRole;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import ui.KanbanBoard;
+import ui.KanbanColumn;
+import ui.KanbanCard;
+
+import static org.junit.Assert.*;
+
+public class CardIntegrationTest extends ApplicationTest
+{
+    private KanbanCard kanbanCard;
+    private KanbanColumn kanbanColumn;
+
+    @Before
+    public void init(){
+        KanbanBoard board = new KanbanBoard();
+        board.getController().fillWithData(new BoardModel("test"));
+
+        kanbanColumn = new KanbanColumn(board);
+        kanbanColumn.getController().fillWithData(new ColumnModel("column"));
+
+        CardModel cardModel = new CardModel("card");
+        kanbanCard = new KanbanCard(kanbanColumn);
+        kanbanCard.getController().fillWithData(cardModel);
+
+        kanbanColumn.getController().makeNewCard(cardModel);
+    }
+
+    @Test 
+    public void testDeleteCard(){
+        kanbanCard.getController().deleteCardDirectly(kanbanCard);
+        assertEquals(0, kanbanColumn.getController().getColumnModel().getCards().size());
+    }
+}
